bf0aed6c5386cc1bf867f714c704cb82ff436b1e
Victor Bett <zakayoochieng@gmail.com>
2024-02-29 19:13:16 +0300

    commiting changes

--- vectorWoodcrafts/urls.py
+++ vectorWoodcrafts/urls.py
@@ -3,8 +3,9 @@ from vectorWoodcrafts import views
 
 urlpatterns = [
     path('', views.index,name='index'),
-    path('login', views.login, name = 'login'),
+    path('user_login', views.user_login, name = 'user_login'),
     path('signup', views.signup, name = 'signup'),
+    path('signout', views.signout, name = 'signout'),
     path('home', views.home, name = 'home'),
     path('products', views.products, name = 'products')
 ]
--- vectorWoodcrafts/views.py
+++ vectorWoodcrafts/views.py
@@ -3,34 +3,31 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
+from django.contrib.auth import authenticate, login as auth_login, logout
 from .models import*
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
-
-def login(request):
+def user_login(request):
     if request.method == 'POST':
-        email = request.POST['email']
+        username = request.POST['username']
         password = request.POST['password']
-
         # Authenticate user
-        user = authenticate(request, email=email, password=password)
-
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             # User is authenticated, log them in
-            login(request, user)
+            auth_login(request, user)
             messages.success(request, "Logged in successfully.")
-            return redirect('home')  # Replace 'home' with your desired redirect URL after login
+            return redirect('index')  # Replace 'home' with your desired redirect URL after login
         else:
             # Invalid login credentials
             messages.error(request, "Invalid email or password.")
-            return redirect('login')  # Replace 'login' with your login URL name
+            return redirect('user_login')  # Replace 'login' with your login URL name
 
     else:
-        return render(request, 'login.html')
-       
+        return render(request, 'user_login.html')     
 
 def signup(request):
     if request.method == 'POST':
@@ -53,12 +50,12 @@ def signup(request):
             messages.error(request, "Passwords do not match.")
             return redirect('signup')
 
-        if User.objects.filter(email=email).exists():
+        if User.objects.filter(username=username).exists():
             messages.error(request, "Email already exists.")
             return redirect('signup')
         
         # Password validation passed, create user
-        user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email)
+        user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email = email)
         user.set_password(password)
         user.save()
 
@@ -66,11 +63,16 @@ def signup(request):
         Customer.objects.create(user=user, phone_no=phone_no) 
 
         messages.success(request, "Account created successfully. You can now log in.")
-        return redirect('login')
+        return redirect('user_login')
     else:
         return render(request, 'signup.html')
 
+def signout(request):
+    logout(request)
+    return redirect('index')
+
 def home(request):
     return render(request, 'home.html')
+    
 def products(request):
     return render(request, 'products.html')
